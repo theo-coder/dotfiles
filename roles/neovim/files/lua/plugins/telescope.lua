47d53c738c9215ba118ce89021c29dce6f53453d
Théo Posty <theo@posty.fr>
2025-05-07 10:12:30 +0200

    add projects to telescope

--- roles/neovim/files/lua/plugins/telescope.lua
+++ roles/neovim/files/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"olacin/telescope-cc.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"ahmedkhalf/project.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
@@ -27,6 +28,7 @@ return {
 		})
 		telescope.load_extension("fzf")
 		telescope.load_extension("conventional_commits")
+		telescope.load_extension("projects")
 
 		local builtin = require("telescope.builtin")
 
@@ -34,6 +36,7 @@ return {
 		vim.keymap.set("n", "<leader>pg", "<cmd>Telescope git_files<cr>")
 		vim.keymap.set("n", "<leader>pb", "<cmd>Telescope buffers<cr>")
 		vim.keymap.set("n", "<leader>pn", "<cmd>Telescope notify<cr>")
+		vim.keymap.set("n", "<leader>pp", "<cmd>Telescope projects<cr>")
 
 		vim.keymap.set("n", "<leader>gc", "<cmd>Telescope conventional_commits<cr>")
 
