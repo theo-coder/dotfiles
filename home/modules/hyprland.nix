ffffef69207d9e7c2d7aed8cf5061029819e5740
Theo Posty <theo@posty.fr>
2025-06-16 07:54:50 +0200

    feat: add monitor script

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -112,6 +112,9 @@
         "suppressevent maximize, class:.*"
         "pin,class:^(rofi)$"
       ];
+      exec = [
+        "~/.local/bin/monitor"
+      ];
       exec-once = [
         "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
